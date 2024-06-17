5010eee713f01b3d956dc40c2031be591ee22987
Abi-Nf <hei.abigail.3@gmail.com>
2024-06-17 15:32:05 +0300

    feat!: Marcheur to instanciate

--- src/main/java/white/walker/Marcheur.java
+++ src/main/java/white/walker/Marcheur.java
@@ -4,7 +4,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Marcheur {
-  public static List<Lieu> marche(Carte carte, Lieu départ, Lieu destination){
+  private final String nom;
+
+  public Marcheur(String nom) {
+    this.nom = nom;
+  }
+
+  public List<Lieu> marche(Carte carte, Lieu départ, Lieu destination){
     List<Lieu> lieux = new ArrayList<>();
     lieux.add(départ);
 
@@ -16,14 +22,24 @@ public class Marcheur {
         .filter(rue -> rue.aCeLieu(dernierAjouté))
         .toList();
       int indexAléatoire = (int) Math.floor(Math.random() * (rueConnecté.size() - 1));
+      if(indexAléatoire == -1) continue;
       var rueAléatoire = rueConnecté.get(indexAléatoire);
-      if (rueAléatoire.getLieuA().equals(dernierAjouté)) {
-        lieux.add(rueAléatoire.getLieuB());
-      }else {
-        lieux.add(rueAléatoire.getLieuA());
-      }
+      var lieuAAller = lieuAAller(rueAléatoire, dernierAjouté, lieux);
+      lieux.add(lieuAAller);
+      carte.verifierUnCulDeSac(rueAléatoire, lieuAAller, destination);
     }
-
     return lieux;
+  }
+
+  private static Lieu lieuAAller(Rue rueAléatoire, Lieu dernierAjouté, List<Lieu> lieux) {
+    if (rueAléatoire.getLieuA().equals(dernierAjouté)) {
+      return rueAléatoire.getLieuB();
+    }
+    return rueAléatoire.getLieuA();
+  }
+
+  @Override
+  public String toString() {
+    return nom;
   }
 }
