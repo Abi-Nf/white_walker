beed3ff0f3e4264f64d1e18b1dbcd12fb9b4ee74
Abi-Nf <hei.abigail.3@gmail.com>
2024-06-17 12:57:14 +0300

    feat: Marcheur

--- src/main/java/white/walker/Marcheur.java
+++ src/main/java/white/walker/Marcheur.java
@@ -1,4 +1,29 @@
 package white.walker;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Marcheur {
+  public static List<Lieu> marche(Carte carte, Lieu départ, Lieu destination){
+    List<Lieu> lieux = new ArrayList<>();
+    lieux.add(départ);
+
+    var rues = carte.lesRues();
+    while(!lieux.getLast().equals(destination)){
+      var dernierAjouté = lieux.getLast();
+      var rueConnecté = rues
+        .stream()
+        .filter(rue -> rue.aCeLieu(dernierAjouté))
+        .toList();
+      int indexAléatoire = (int) Math.floor(Math.random() * (rueConnecté.size() - 1));
+      var rueAléatoire = rueConnecté.get(indexAléatoire);
+      if (rueAléatoire.getLieuA().equals(dernierAjouté)) {
+        lieux.add(rueAléatoire.getLieuB());
+      }else {
+        lieux.add(rueAléatoire.getLieuA());
+      }
+    }
+
+    return lieux;
+  }
 }
