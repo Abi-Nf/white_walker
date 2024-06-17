410eba837e686f0505e6005c838900b8c3ef97bf
Abi-Nf <hei.abigail.3@gmail.com>
2024-06-17 12:11:39 +0300

    feat: Carte

--- src/main/java/white/walker/Carte.java
+++ src/main/java/white/walker/Carte.java
@@ -1,4 +1,24 @@
 package white.walker;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class Carte {
+  private final Set<Rue> ensembleDeRue = new HashSet<>();
+
+  public void ajouterUneRue(Rue rue) {
+    ensembleDeRue.add(rue);
+  }
+
+  public void ajouterUneRue(String nom, Lieu a, Lieu b){
+    ensembleDeRue.add(new Rue(nom, a, b));
+  }
+
+  public void ajouterUneRue(Lieu a, Lieu b){
+    ensembleDeRue.add(new Rue(a, b));
+  }
+
+  public Set<Rue> lesRues() {
+    return ensembleDeRue;
+  }
 }
