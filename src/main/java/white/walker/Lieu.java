6f9138a2c6405e29a42518f66132ede615b1867d
Abi-Nf <hei.abigail.3@gmail.com>
2024-06-18 12:36:02 +0300

    chore: rename ruesConnecte field to lesRuesPossible

--- src/main/java/white/walker/Lieu.java
+++ src/main/java/white/walker/Lieu.java
@@ -4,7 +4,7 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class Lieu {
-  private final Set<Rue> ruesConnecte = new HashSet<>();
+  private final Set<Rue> lesRuesPossible = new HashSet<>();
 
   private final String nom;
 
@@ -13,11 +13,11 @@ public class Lieu {
   }
 
   void connecterA(Rue rue){
-    ruesConnecte.add(rue);
+    lesRuesPossible.add(rue);
   }
 
   public boolean estConnecterA(Rue rue){
-    return ruesConnecte.contains(rue);
+    return lesRuesPossible.contains(rue);
   }
 
   @Override
